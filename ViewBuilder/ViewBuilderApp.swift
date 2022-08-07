acf688f79cbb06d61992fb24bb681ee7beee92fb
Christian Skorobogatow <christiansko@icloud.com>
2022-08-07 20:10:35 +0200

    Initial Commit

---
+++ ViewBuilder/ViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderApp.swift
+//  ViewBuilder
+//
+//  Created by Christian Skorobogatow on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
